0fbd82178d3903597547982de77050a73054be18
Nathan Sun <nathan.sunyt@gmail.com>
2024-12-17 11:17:29 +0100

    feat (jest config): add coverageReporters + coverageFrom

--- jest.config.js
+++ jest.config.js
@@ -7,4 +7,9 @@ module.exports = {
     '^@/(.*)$': '<rootDir>/src/$1',
   },
   setupFilesAfterEnv: ['<rootDir>/src/test/setup.ts'],
+  coverageReporters: ['html', 'text'],
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx}', // Include all TypeScript files in the src directory
+    '!src/**/*.d.ts', // Exclude TypeScript declaration files
+  ],
 };
