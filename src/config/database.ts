1dd29130852887422dcd94decf505b20dd6ee310
Nathan Sun <nathan.sunyt@gmail.com>
2024-12-18 15:47:15 +0100

    fix: edit database.ts for test db + add docker service for test db

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,12 +1,19 @@
-import { Pool } from 'pg';
+import {Pool} from 'pg';
 import dotenv from 'dotenv';
 
-dotenv.config();
+if (process.env.NODE_ENV === 'test') {
+    console.log('Using test environment');
+    dotenv.config({path: '.env.test'});
+} else {
+    console.log('Using ', process.env.NODE_ENV, ' environment');
+
+    dotenv.config();
+}
 
 export const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: parseInt(process.env.DB_PORT || '5432'),
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: parseInt(process.env.DB_PORT || '5432'),
 });
